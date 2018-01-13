28275a7ed9dd8f68d58af03ab33456ba640ed219
Kyle Alwyn <kyle.j.alwyn@gmail.com>
2018-01-13 12:25:05 -0800

    Configure RSS feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Blog',
-    description: 'A starter blog demonstrating what Gatsby can do.',
+    title: "Ashley Worsham Portfolio",
+    description: 'The personal portfolio of Ashley Worsham, a UX designer based in San Francisco',
     siteUrl: 'https://ashleyworsham.com',
   },
   pathPrefix: '/',
@@ -45,7 +45,60 @@ module.exports = {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
       },
     },
-    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allContentfulPost } }) => {
+              return allContentfulPost.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  title: edge.node.title,
+                  date: edge.node.updatedAt,
+                  description: edge.node.description,
+                  url: site.siteMetadata.siteUrl + edge.node.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.slug,
+                  custom_elements: [{ "content:encoded": edge.node.content }],
+                });
+              });
+            },
+            query: `
+              {
+                allContentfulPost(
+                  limit: 1000
+                ) {
+                  edges {
+                    node {
+                      title
+                      description
+                      slug
+                      content {
+                        content
+                      }
+                      createdAt
+                      updatedAt
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/feed.xml",
+          },
+        ],
+      },
+    },
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
   ],
