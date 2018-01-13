ffe7c1f26d54e1316808dcc725ed928ffe0b1137
Kyle Alwyn <kyle.j.alwyn@gmail.com>
2018-01-13 13:10:59 -0800

    Add styled-components gatsby plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   },
   pathPrefix: '/',
   plugins: [
+    'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-source-contentful`,
       options: {
--- src/templates/post.js
+++ src/templates/post.js
@@ -9,9 +9,6 @@ export default class PostTemplate extends React.Component {
   render() {
     const post = this.props.data.contentfulPost;
 
-    const { previous, next } = this.props.pathContext
-
-    console.log(post)
     return (
       <div>
         <Helmet title={post.title} />
