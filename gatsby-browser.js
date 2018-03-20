103cb4a349b0c18f920330eaa0073c673e4c49d7
Kyle Alwyn <kjalwyn@gmail.com>
2018-03-19 23:16:09 -0700

    Remove smoke test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,7 +4,6 @@ ReactGA.initialize('UA-75725762-2');
 
 exports.onRouteUpdate = ({ location }) => {
   if (process.env.NODE_ENV === 'production') {
-    console.log('Pageview');
     ReactGA.pageview(location.pathname);
   }
 };
