0656bd363e72d9df1b49c7dfc475c7af26cc7c68
Kyle Alwyn <kjalwyn@gmail.com>
2018-03-19 23:13:24 -0700

    Quick smoke test

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@ ReactGA.initialize('UA-75725762-2');
 
 exports.onRouteUpdate = ({ location }) => {
   if (process.env.NODE_ENV === 'production') {
+    console.log('Pageview');
     ReactGA.pageview(location.pathname);
   }
 };
